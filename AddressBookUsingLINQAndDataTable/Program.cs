a0bb95cff74fac130a8ce2b63391f01b3e36e1b0
ArtiDandge <58045752+ArtiDandge@users.noreply.github.com>
2021-01-04 19:25:50 +0530

    [Arti Dandge] Add : Display Welcome Message on Console

---
+++ AddressBookUsingLINQAndDataTable/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AddressBookUsingLINQAndDataTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Address Book System using Linq and DataTable");
+        }
+    }
+}
