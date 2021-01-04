1543d5eab366d1c6e603405449496afdfebf2c90
ArtiDandge <58045752+ArtiDandge@users.noreply.github.com>
2021-01-04 22:02:58 +0530

    [Arti Dandge] Add : Ability to Add Fields//Columns to AddressBook DataTable

--- AddressBookUsingLINQAndDataTable/AddressBookDataTable.cs
+++ AddressBookUsingLINQAndDataTable/AddressBookDataTable.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Data;
+using System.Linq;
 using System.Collections.Generic;
 using System.Text;
 
@@ -7,6 +8,22 @@ namespace AddressBookUsingLINQAndDataTable
 {
     public class AddressBookDataTable
     {
-        DataTable addressBookTable = new DataTable();
+        /// <summary>
+        /// Method to Create AddressBook DataTable
+        /// </summary>
+        public void CreateAddressBookDataTable()
+        {
+            DataTable addressBookTable = new DataTable();
+
+            //Add Columns to DataTable
+            addressBookTable.Columns.Add("FirstName", typeof(string));
+            addressBookTable.Columns.Add("LastName", typeof(string));
+            addressBookTable.Columns.Add("Address", typeof(string));
+            addressBookTable.Columns.Add("City", typeof(string));
+            addressBookTable.Columns.Add("State", typeof(string));
+            addressBookTable.Columns.Add("Zip", typeof(int));
+            addressBookTable.Columns.Add("PhoneNumber", typeof(long));
+            addressBookTable.Columns.Add("Email", typeof(string));
+        }
     }
 }
